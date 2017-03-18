f0218a36f235a9277008fc160b377099437149d4
William Farley <williamfarley@Williams-MacBook-Pro.local>
2017-03-18 17:21:37 -0400

    V2

--- Drink Much? V2/GameViewController1.swift
+++ Drink Much? V2/GameViewController1.swift
@@ -9,10 +9,48 @@
 import UIKit
 
 class GameViewController1: UIViewController {
-
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    @IBOutlet weak var tryAgainButton: UIButton!
+        
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var sessionHighScoreLabel: UILabel!
+    @IBOutlet weak var careerHighScoreLabel: UILabel!
+    
+    var round = 0
+    var score = 0
+    var selectedButton: Int = -1
+    var lastButton: Int = -1
+    
+    var careerHighScore: Int?
+    var sessionHighScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if careerHighScore != 0 {
+            careerHighScoreLabel.text = "Career High Score: \(careerHighScore)"
+        } else {
+            careerHighScore = 0
+            careerHighScoreLabel.isHidden = true
+        }
+        
+        if sessionHighScore != 0 {
+            sessionHighScoreLabel.text = "Session High Score: \(sessionHighScore)"
+        } else {
+            sessionHighScoreLabel.isHidden = true
+        }
+        
+        tryAgainButton.isHidden = true
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +59,156 @@ class GameViewController1: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ViewController" {
+            let ViewController = segue.destination as? ViewController
+            let highScoreDummy = sender as? Int
+            ViewController?.highScore = highScoreDummy!
+        }
     }
-    */
+    
+    //MARK:- Functions
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func stopGame() {
+        timeLabel.text = "Game Over"
+        button1.isEnabled = false
+        button2.isEnabled = false
+        button3.isEnabled = false
+        
+        if score > sessionHighScore {
+            if score > careerHighScore! {
+                careerHighScore = score
+                sessionHighScore = score
+                scoreLabel.text = "New Career High Score: \(score)!"
+            } else if score > sessionHighScore {
+                sessionHighScore = score
+                    scoreLabel.text = "New Session High Score: \(score)!"
+            } else {
+            scoreLabel.text = "Your Score: \(score)" }
+        }
+        
+        careerHighScoreLabel.isHidden = false
+        careerHighScoreLabel.text = "Career High Score: \(careerHighScore!)"
+        sessionHighScoreLabel.isHidden = false
+        sessionHighScoreLabel.text = "Session High Score: \(sessionHighScore)"
+        
+        tryAgainButton.isHidden = false
+    }
 
+    func checkButtonColor(_ selectedButton: Int) {
+        
+        switch selectedButton {
+        case 0:
+            if lastButton == -1 || lastButton == 0 {
+                score = score + 1
+            } else {
+                stopGame()
+                showAlert(title: "Game Over", message: "You Hit A Red Button!")
+            }
+        case 1:
+            if lastButton == -1 || lastButton == 1 {
+                score = score + 1
+            } else {
+                stopGame()
+                showAlert(title: "Game Over", message: "You Hit A Red Button!")
+            }
+        case 2:
+            if lastButton == -1 || lastButton == 2 {
+                score = score + 1
+            } else {
+                stopGame()
+                showAlert(title: "Game Over", message: "You Hit A Red Button!")
+            }
+        default:
+            print("Something went wrong")
+        }
+    }
+    
+    func newRound() {
+        label1.backgroundColor = UIColor.red
+        label2.backgroundColor = UIColor.red
+        label3.backgroundColor = UIColor.red
+        round = round + 1
+        if round == 1 {
+            timeLabel.text = "Your 15 Seconds Have Started!"
+            Timer.scheduledTimer(timeInterval: 15, target: self, selector: #selector(GameViewController1.stopGame), userInfo: nil, repeats: false)
+        }
+    }
+    
+    func randomButton() {
+        var randomNumber: Int
+        
+        repeat {
+            randomNumber = Int(arc4random_uniform(3))
+        } while randomNumber == lastButton
+        
+        switch randomNumber {
+        case 0:
+            label1.backgroundColor = UIColor.green
+        case 1:
+            label2.backgroundColor = UIColor.green
+        case 2:
+            label3.backgroundColor = UIColor.green
+        default:
+            print("Something went wrong")
+        }
+        
+        lastButton = randomNumber
+    }
+    
+    // MARK:- Actions
+    
+    @IBAction func returnButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func button1Pressed(_ sender: UIButton) {
+        instructionsLabel.text = "Press The Green Button!"
+        checkButtonColor(0)
+        scoreLabel.text = "Score: \(score)"
+        newRound()
+        randomButton()
+    }
+
+    @IBAction func button2Pressed(_ sender: UIButton) {
+        instructionsLabel.text = "Press The Green Button!"
+        checkButtonColor(1)
+        scoreLabel.text = "Score: \(score)"
+        newRound()
+        randomButton()
+    }
+
+    @IBAction func button3Pressed(_ sender: UIButton) {
+        instructionsLabel.text = "Press The Green Button!"
+        checkButtonColor(2)
+        scoreLabel.text = "Score: \(score)"
+        newRound()
+        randomButton()
+    }
+
+    @IBAction func tryAgainPressed(_ sender: UIButton) {
+        button1.isEnabled = true
+        button2.isEnabled = true
+        button3.isEnabled = true
+        label1.backgroundColor = UIColor.green
+        label2.backgroundColor = UIColor.green
+        label3.backgroundColor = UIColor.green
+        score = 0
+        round = 0
+        lastButton = -1
+        scoreLabel.text = "Score:"
+        sessionHighScoreLabel.text = "Session High Score: \(sessionHighScore)"
+        careerHighScoreLabel.text = "Career High Score: \(careerHighScore!)"
+        instructionsLabel.text = "Press Any Button To Start"
+        tryAgainButton.isHidden = true
+        timeLabel.text = "Once You Start, You Have 15 Seconds"
+    }
+    
 }
--- Drink Much? V2/ViewController.swift
+++ Drink Much? V2/ViewController.swift
@@ -10,104 +10,155 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var drinkerImage: UIImageView!
     @IBOutlet weak var drinkCountLabel: UILabel!
-    @IBOutlet weak var bacLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var hoursLabel: UILabel!
+    @IBOutlet weak var resetLabel: UIButton!
+    @IBOutlet weak var bacLabel: UILabel!
     
-    var weightTollerance: Double = 0.02
+    var weightTollerance: Double = 0.0
     var drinkCount = 0
     var relativeDrinkCount: Double = 0
     var BAC: Double = 0
     
+    var highScore = 0
+    
     var firstDrinkTime = 0.0
+    var liabilityWaiverAccepted: Bool = false
     
-    let messages = ["This Won't Pop Up",
-                    "Let's Get It Started!",
-                    "Who Has a Speaker?",
-                    "Please Don't Puke Yet",
-                    "Numero Quattro: Start of the Happy Zone",
-                    "Don't You Fucking Think of Driving",
-                    "Really Getting There Now",
-                    "New Bar! Someone Call The Uber",
-                    "Hit Up Your Side-Piece Yet?",
-                    "Hit Up Your Ex Yet?",
-                    "Give Up Your Phone",
+    var defaultsData = UserDefaults.standard
+    
+    let messages = ["I Solemnly Swear That I Am Up to No Good",
+                    "That's A Start",
+                    "Two Down. I told you I can drink, Mom",
+                    "Third Time's The Charm",
+                    "Hey, Look! I think we just crossed the legal limit!",
+                    "Cinco De Cerveza!",
+                    "What time is it? Sex O'Clock? lol",
+                    "Why was six afraid of seven?",
+                    "I don't know. I didn't take Psych",
                     "Ho Gome You're Drunk",
-                    "Nah Nevermind You Have Nothing to Do in The Morning",
-                    "Ok Either You're Just Scrolling Through",
-                    "Or You're A Savage"]
-    
-    let timeInvervals = [3600,
-                         7200,
-                         10800,
-                         14400,
-                         18000,
-                         21600,
-                         25200,
-                         28800,
-                         32400,
-                         36000,
-                         39600,
-                         43200]
+                    "Actually Just Clear Your Schedule for the Morning",
+                    "Man, You're Getting Fucked Tonight",
+                    "Guess What Number You're At. Wait, don't look.",
+                    "No, This is Number 13... You Might Have a Problem",
+                    "Give Someone Your Phone. It's Dangerous"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        firstDrinkTime = NSTimeIntervalSince1970
+        
+        weightTollerance = defaultsData.double(forKey: "weightTollerance")
+        
+        drinkCount = defaultsData.integer(forKey: "drinkCount")
+        
+        defaultsData.set(drinkCount, forKey: "drinkCount")
+        
+        relativeDrinkCount = defaultsData.double(forKey: "relativeDrinkCount")
+        
+        liabilityWaiverAccepted = defaultsData.bool(forKey: "liabilityWaiverAccepted")
+        
+        highScore = defaultsData.integer(forKey: "careerHighScore")
+        
+        if highScore == 0 {
+            defaultsData.set(highScore, forKey: "careerHighScore")
+        }
+        
+        BAC = relativeDrinkCount * weightTollerance
+        
+        updateLabels()
+        
+        if drinkCount == 0 {
+            resetLabel.isHidden = true
+        } else {
+            resetLabel.isHidden = false
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: Functions
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "GameViewController1" {
+            let secondViewController = segue.destination as? GameViewController1
+            let highScoreDummy = sender as? Int
+            secondViewController?.careerHighScore = highScoreDummy
+        }
+    }
+    
+    // MARK: - Functions
+    
+    func showAlert(title: String, message: String, dismissMessage: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: dismissMessage, style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func updateLabels() {
+        drinkCountLabel.text = "Drink Count: \(drinkCount)"
+        bacLabel.text = "Expected BAC: \(BAC)"
+        messageLabel.text = messages[drinkCount]
+    }
     
     func timer() {
         let currentTime = NSTimeIntervalSince1970
         let elapsedTime = Int(currentTime - firstDrinkTime)
+
+        let timeInvervals = [3600,
+                             7200,
+                             10800,
+                             14400,
+                             18000,
+                             21600,
+                             25200,
+                             28800,
+                             32400,
+                             36000,
+                             39600,
+                             43200]
         
         switch elapsedTime {
-        case let elapsedTime where timeInvervals[0] > elapsedTime:
+        case 0 ..< timeInvervals[0]:
             hoursLabel.text = "Hours Elapsed: Less Than 1"
-        case let elapsedTime where timeInvervals[1] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 1
+        case timeInvervals[0] ..< timeInvervals[1]:
+            relativeDrinkCount = Double(drinkCount) - 1
             hoursLabel.text = "Hours Elapsed: 1"
-        case let elapsedTime where timeInvervals[2] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 2
+        case timeInvervals[1] ..< timeInvervals[2]:
+            relativeDrinkCount = Double(drinkCount) - 2
             hoursLabel.text = "Hours Elapsed: 2"
-        case let elapsedTime where timeInvervals[3] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 3
+        case timeInvervals[2] ..< timeInvervals[3]:
+            relativeDrinkCount = Double(drinkCount) - 3
             hoursLabel.text = "Hours Elapsed: 3"
-        case let elapsedTime where timeInvervals[4] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 4
+        case timeInvervals[3] ..< timeInvervals[4]:
+            relativeDrinkCount = Double(drinkCount) - 4
             hoursLabel.text = "Hours Elapsed: 4"
-        case let elapsedTime where timeInvervals[5] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 5
+        case timeInvervals[4] ..< timeInvervals[5]:
+            relativeDrinkCount = Double(drinkCount) - 5
             hoursLabel.text = "Hours Elapsed: 5"
-        case let elapsedTime where timeInvervals[6] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 6
+        case timeInvervals[5] ..< timeInvervals[6]:
+            relativeDrinkCount = Double(drinkCount) - 6
             hoursLabel.text = "Hours Elapsed: 6"
-        case let elapsedTime where timeInvervals[7] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 7
+        case timeInvervals[6] ..< timeInvervals[7]:
+            relativeDrinkCount = Double(drinkCount) - 7
             hoursLabel.text = "Hours Elapsed: 7"
-        case let elapsedTime where timeInvervals[8] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 8
+        case timeInvervals[7] ..< timeInvervals[8]:
+            relativeDrinkCount = Double(drinkCount) - 8
             hoursLabel.text = "Hours Elapsed: 8"
-        case let elapsedTime where timeInvervals[9] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 9
+        case timeInvervals[8] ..< timeInvervals[9]:
+            relativeDrinkCount = Double(drinkCount) - 9
             hoursLabel.text = "Hours Elapsed: 9"
-        case let elapsedTime where timeInvervals[10] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 10
+        case timeInvervals[9] ..< timeInvervals[10]:
+            relativeDrinkCount = Double(drinkCount) - 10
             hoursLabel.text = "Hours Elapsed: 10"
-        case let elapsedTime where timeInvervals[11] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 11
+        case timeInvervals[10] ..< timeInvervals[11]:
+            relativeDrinkCount = Double(drinkCount) - 11
             hoursLabel.text = "Hours Elapsed: 11"
-        case let elapsedTime where timeInvervals[12] > elapsedTime:
-            relativeDrinkCount = relativeDrinkCount - 12
+        case timeInvervals[11] ..< timeInvervals[12]:
+            relativeDrinkCount = Double(drinkCount) - 12
             hoursLabel.text = "Hours Elapsed: 12"
         default:
             hoursLabel.text = "Hours Elapsed: Greater Than 12"
@@ -115,40 +166,84 @@ class ViewController: UIViewController {
 
     }
     
-    // MARK: Actions
-
-    @IBAction func weightEntered(_ sender: UITextField) {
-        
-        if let enteredWeight = Double(textField.text!) {
-            weightTollerance = 0.01 * (4 - (0.01 * enteredWeight)) }
-        else {
-            let alertController = UIAlertController(title: "Entry Error", message: "Please enter a valid number. Not an empty string. No commas, symbols, or non-numeric characters.", preferredStyle: .alert)
-            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alertController.addAction(defaultAction)
-            present(alertController, animated: true, completion: nil) }
-        
+    func markFirstDrinkTime() {
+        if drinkCount == 1 {
+            firstDrinkTime = NSTimeIntervalSince1970
+            defaultsData.set(firstDrinkTime, forKey: "firstDrinkTime")
+            if liabilityWaiverAccepted == false {
+                showAlert(title: "Terms And Conditions", message: "You Can Read Our Terms And Conditions on Our Website (insert link). Press Accept to Continue", dismissMessage: "Accept")
+                liabilityWaiverAccepted = true
+                defaultsData.set(true, forKey: "liabilityWaiverAccepted")
+            }
+        }
     }
     
-    @IBAction func drinkAdded(_ sender: UITapGestureRecognizer) {
+    func checkIfWeightInputted() {
+        if weightTollerance == 0.0 {
+            weightTollerance = 0.02
+            showAlert(title: "Please Enter Weight in User Info", message: "For Now We'll Just Set You to 0.02 Per Drink.", dismissMessage: "Ok")
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func addDrinkPressed(_ sender: UIButton) {
+        
+        resetLabel.isHidden = false
         
         drinkCount = drinkCount + 1
         
+        markFirstDrinkTime()
+        
+        checkIfWeightInputted()
+        
         relativeDrinkCount = relativeDrinkCount + 1
+        
+        if relativeDrinkCount == 14 {
+            showAlert(title: "That's As High As We Go", message: "Sorry if you're actually still drinking.", dismissMessage: "Ok")
+        }
         
         BAC = relativeDrinkCount * weightTollerance
         
-        drinkCountLabel.text = "Drink Count: \(drinkCount)"
-        
-        bacLabel.text = "\(BAC)"
-        
-        messageLabel.text = messages[drinkCount]
+        updateLabels()
         
         timer()
         
-        }
-    
-    
+        defaultsData.synchronize()
+
     }
 
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        drinkCount = 0
+        relativeDrinkCount = 0
+        defaultsData.synchronize()
+        drinkCountLabel.text = "Drink Count: "
+        bacLabel.text = "Expected BAC: "
+        messageLabel.text = messages[0]
+        hoursLabel.text = "Hours Elapsed: "
+        resetLabel.isHidden = true
+    }
+    
+    @IBAction func coordinationPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "GameViewController1", sender: highScore)
+    }
+    
+    @IBAction func settingsPressed(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "UserDetails", sender: nil)
+    }
+    
+    @IBAction func unwindToViewController(sender: UIStoryboardSegue) {
+        if let gameViewController = sender.source as? GameViewController1 {
+            highScore = gameViewController.careerHighScore!
+        }
+        
+        if let detailViewController = sender.source as? UserDetailViewController {
+            weightTollerance = detailViewController.weightTollerance
+        }
+        
+        defaultsData.synchronize()
+    }
+
+}
 
 
